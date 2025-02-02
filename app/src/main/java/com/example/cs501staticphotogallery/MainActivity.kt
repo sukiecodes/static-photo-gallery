1371b4cdc6a31837d34ec125c5bafc58892d8c7f
Sukie Zhang <sukiezhang.sz@gmail.com>
2025-02-02 00:40:42 -0500

    created PhotoGallery func and added the images in a 6x6 grid

--- app/src/main/java/com/example/cs501staticphotogallery/MainActivity.kt
+++ app/src/main/java/com/example/cs501staticphotogallery/MainActivity.kt
@@ -6,11 +6,17 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.cs501staticphotogallery.ui.theme.CS501StaticPhotoGalleryTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +26,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             CS501StaticPhotoGalleryTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    PhotoGallery(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +36,58 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun PhotoGallery(modifier: Modifier = Modifier) {
+    val imageSize = modifier.size(100.dp)
+    Column {
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Image(
+                painter = painterResource(R.drawable.calcifer),
+                contentDescription = null,
+                modifier = imageSize
+            )
+
+            Image(
+                painter = painterResource(R.drawable.jiji),
+                contentDescription = null,
+                modifier = imageSize
+            )
+        }
+
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Image(
+                painter = painterResource(R.drawable.kiki),
+                contentDescription = null,
+                modifier = imageSize
+            )
+
+            Image(
+                painter = painterResource(R.drawable.ponyo),
+                contentDescription = null,
+                modifier = imageSize
+            )
+        }
+
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Image(
+                painter = painterResource(R.drawable.radish),
+                contentDescription = null,
+                modifier = imageSize
+            )
+
+            Image(
+                painter = painterResource(R.drawable.totoro),
+                contentDescription = null,
+                modifier = imageSize
+            )
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun PhotoGalleryPreview() {
     CS501StaticPhotoGalleryTheme {
-        Greeting("Android")
+        PhotoGallery()
     }
 }
